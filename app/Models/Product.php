66f5bfa67e2bf4584cb44cf1b0e1d37220c9c3ed
wangyaoguang <wangyaoguang@myhayo.com>
2019-05-25 16:43:13 +0800

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,8 +26,19 @@ class Product extends Model
      */
     public static function transferIconUrl($icon_url)
     {
+        return self::getImageHostPrefix() . '/' . $icon_url;
+    }
+
+
+    /**
+     * 获取图片域名服务器前缀地址
+     *
+     * @return \Illuminate\Config\Repository|mixed
+     */
+    public static function getImageHostPrefix()
+    {
         $disk = config('admin.upload.disk');
 
-        return config("filesystems.disks.{$disk}.url") . '/' . $icon_url;
+        return config("filesystems.disks.{$disk}.url");
     }
 }
